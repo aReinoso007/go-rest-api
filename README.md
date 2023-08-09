f5d1df8fbfccb01db965608d3ce7095f520a8813
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-08-08 23:54:24 -0500

    installing gin and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Go & Gin Tutotial
+1. Install GIN
+```bash
+go get -u github.com/gin-gonic/gin
+```
+then import it in the class:
+```go
+import "github.com/gin-gonic/gin"
+```
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 // since this is standalone program (oposed to a library)
 package main
 
+import "github.com/gin-gonic/gin"
+
 type album struct {
-	//this `json:"id" is for serialization`
-	ID string `json:"id"`
+	//this `json:"id" is for serialization` so to not use the capitalized prop
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+
 }
